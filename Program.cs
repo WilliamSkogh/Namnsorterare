537bd1efa01660126963c6ab74f3b52f87f0ed33
William Skogh <skogh22@gmail.com>
2024-10-01 16:22:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using
+System.Collections.Generic;
+using System.Xml.Linq;
+namespace NameSorter
+class Program
+{
+{
+{
+static void Main(string[] args)
+List<string> names = new List<string> { "Anna", "John", "Alice", "Bo Console.WriteLine("Original list:");
+foreach (var name in names)
+{
+}
+Console.WriteLine(name);
+names.Sort(); // Sort the names alphabetically Console.WriteLine("\nSorted list:");
+foreach (var name in names)
+{
+}
+Console.WriteLine(name);
+Console.WriteLine("\nEnter name to search: ");
+string searchName Console.ReadLine();
+if (names.Contains(searchName))
+Console.WriteLine($"{searchName} is in the list.");
+}
+}
+{
+}
+else
+{
+}
+Console.WriteLine($"{searchName} is not in the list.");
+Console.ReadKey();
